eee7fa4ed87cb486d73dac7b8db56bcdd38bfb26
osigie <kenosagie88@gmail.com>
2025-11-12 04:39:40 +0100

    initial setup

---
+++ src/main/java/com/osigie/rehook/RehookApplication.java
@@ -0,0 +1,13 @@
+package com.osigie.rehook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RehookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osigie/rehook/RehookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osigie.rehook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
