62e5a06e5053dce0398294560b01e4794457897e
osigie <kenosagie88@gmail.com>
2025-11-13 07:30:01 +0100

    feat(EndpointController): add endpoint controller to test tenancy

--- src/main/java/com/osigie/rehook/controller/EndpointController.java
+++ src/main/java/com/osigie/rehook/controller/EndpointController.java
@@ -1,4 +1,23 @@
 package com.osigie.rehook.controller;
 
+import com.osigie.rehook.model.Endpoint;
+import com.osigie.rehook.repository.EndpointRepository;
+import com.osigie.rehook.service.EndpointService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/endpoints")
 public class EndpointController {
+private final EndpointService endpointService;
+    public EndpointController(EndpointService endpointService) {
+        this.endpointService = endpointService;
+    }
+
+    @PostMapping
+    public Endpoint save(@RequestBody Endpoint endpoint) {
+       return endpointService.save(endpoint);
+    }
 }
