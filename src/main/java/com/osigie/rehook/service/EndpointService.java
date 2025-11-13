bf9d7310501dd4e2b68044aba5a347837ade80ec
osigie <kenosagie88@gmail.com>
2025-11-13 07:32:00 +0100

    add service and repo

--- src/main/java/com/osigie/rehook/service/EndpointService.java
+++ src/main/java/com/osigie/rehook/service/EndpointService.java
@@ -1,4 +1,12 @@
-package com.osigie.rehook.service.impl;
+package com.osigie.rehook.service;
+
+import com.osigie.rehook.model.Endpoint;
+
+import java.util.UUID;
 
 public interface EndpointService {
+
+    Endpoint findById(UUID id);
+    Endpoint save(Endpoint endpoint);
+
 }
