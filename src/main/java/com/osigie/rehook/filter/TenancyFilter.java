dac9f72b2d21f25d6371db8a520bf30e356f7ba7
osigie <kenosagie88@gmail.com>
2025-11-13 07:31:19 +0100

    feat(TenancyFilter): add TODO, to validate the tenancy passed from the endpoint or header

--- src/main/java/com/osigie/rehook/filter/TenancyFilter.java
+++ src/main/java/com/osigie/rehook/filter/TenancyFilter.java
@@ -16,12 +16,11 @@ public class TenancyFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-//TODO: validate the tenant and also create a specail tenant retrieval for webhook endpoint through secret
+//TODO: validate the tenant and also create a special tenant retrieval for webhook endpoint through secret
         log.info("Request URL: {} ", httpRequest.getRequestURI());
         String tenantId = httpRequest.getHeader("x-tenant-id");
 
         TenantContext.set(TenantContext.builder().tenantId(tenantId).build());
-
         filterChain.doFilter(servletRequest, servletResponse); // Continue the filter chain
 
         log.info("Tenant Cleared {}", TenantContext.get().getTenantId());
