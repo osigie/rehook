df8862b4cf040c9889c130daa850eaf9f0b60f97
osigie <kenosagie88@gmail.com>
2025-11-13 08:00:53 +0100

    add relationships

--- src/main/java/com/osigie/rehook/model/DeliveryAttempt.java
+++ src/main/java/com/osigie/rehook/model/DeliveryAttempt.java
@@ -2,7 +2,11 @@ package com.osigie.rehook.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
+import java.time.OffsetDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Entity
@@ -18,6 +22,20 @@ public class DeliveryAttempt extends BaseModel {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
+
+    @Column(name="executed_at")
+    private OffsetDateTime executedAt;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb", name = "response_body")
+    private Map<String, String> responseBody;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb", name = "response_headers")
+    private Map<String, String> responseHeaders;
+
+    @Column(name= "duration")
+    private Integer duration;
 
     @Builder
     public DeliveryAttempt(int statusCode, Delivery delivery) {
