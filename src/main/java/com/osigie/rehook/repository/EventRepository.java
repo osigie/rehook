b548fe735726818eb3aab463c5fd6f636bfadd4a
osigie <kenosagie88@gmail.com>
2025-11-20 16:13:09 +0100

    feat(repository): add custom method to delivery repo

--- src/main/java/com/osigie/rehook/repository/EventRepository.java
+++ src/main/java/com/osigie/rehook/repository/EventRepository.java
@@ -1,6 +1,6 @@
 package com.osigie.rehook.repository;
 
-import com.osigie.rehook.model.Event;
+import com.osigie.rehook.domain.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
