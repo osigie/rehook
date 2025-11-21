32e66f8c9ff4c1092820c0121082850a87ae4ebd
osigie <kenosagie88@gmail.com>
2025-11-21 11:14:31 +0100

    add idempotency using SHA-256 and add constraint of subscription_id and idempotency_key

--- src/main/java/com/osigie/rehook/service/impl/IngestionServiceImpl.java
+++ src/main/java/com/osigie/rehook/service/impl/IngestionServiceImpl.java
@@ -9,17 +9,22 @@ import com.osigie.rehook.repository.DeliveryRepository;
 import com.osigie.rehook.repository.EventRepository;
 import com.osigie.rehook.service.IngestionService;
 import com.osigie.rehook.service.SubscriptionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationAdapter;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.OffsetDateTime;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
+@Slf4j
 public class IngestionServiceImpl implements IngestionService {
     private final SubscriptionService subscriptionService;
     private final EventRepository eventRepository;
@@ -39,7 +44,7 @@ public class IngestionServiceImpl implements IngestionService {
     public void ingest(String ingestionId, String payload, Map<String, String> headers) {
 
         /**
-         * TODO: handle indempotency in case there is a retry from client
+         *
          * 1. Get the subscription with the ingestion id if not found, throw not found error
          * 2. Perform security validation of the payload based on the verification method on the subscription
          * 3. Create event and delivery in a transaction and enqueue a delivery*/
@@ -47,12 +52,26 @@ public class IngestionServiceImpl implements IngestionService {
 
         // 1.
         Subscription subscription = subscriptionService.findByIngestionId(ingestionId);
+        String idempotencyKey = this.generateIdempotencyKey(ingestionId, payload);
+
+        Optional<Event> exitingEvent = eventRepository.findBySubscriptionIdAndIdempotencyKey(subscription.getId(), idempotencyKey);
+
+        if (exitingEvent.isPresent()) {
+            log.info("Duplicate event detected for subscription {}, idempotency key: {}",
+                    subscription.getId(), idempotencyKey);
+            return;
+        }
+
 
 //       2. TODO perform validation using strategy pattern based on verification type
 
 //        3.
-        Event event = eventRepository.save(Event.builder().receivedAt(OffsetDateTime.now()).payload(payload).
-                headers(headers).subscription(subscription).build());
+        Event event = eventRepository.save(Event.builder()
+                .idempotencyKey(idempotencyKey)
+                .receivedAt(OffsetDateTime.now())
+                .payload(payload).
+                headers(headers)
+                .subscription(subscription).build());
 
 
         List<Delivery> deliveries = subscription.getEndpoints().stream()
@@ -69,5 +88,16 @@ public class IngestionServiceImpl implements IngestionService {
 
         applicationEventPublisher
                 .publishEvent(new DeliveriesCreatedEvent(savedDeliveries.stream().map((delivery -> delivery.getId())).toList()));
+    }
+
+    private String generateIdempotencyKey(String ingestionId, String payload) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            String combined = ingestionId + ":" + payload;
+            byte[] hash = digest.digest(combined.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
