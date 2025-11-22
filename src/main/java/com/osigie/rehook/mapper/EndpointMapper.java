3b1632af0c145c2f7c37cf30499a04ceca061686
osigie <kenosagie88@gmail.com>
2025-11-22 07:08:18 +0100

    feat(mapper): add EndpointMapper and SubscriptionMapper

--- src/main/java/com/osigie/rehook/mapper/EndpointMapper.java
+++ src/main/java/com/osigie/rehook/mapper/EndpointMapper.java
@@ -1,4 +1,28 @@
 package com.osigie.rehook.mapper;
 
-public class EndpointMapper {
+import com.osigie.rehook.configuration.MapstructMapperConfig;
+import com.osigie.rehook.domain.model.Endpoint;
+import com.osigie.rehook.dto.request.EndpointRequestDto;
+import com.osigie.rehook.dto.response.EndpointResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+
+@Mapper(config = MapstructMapperConfig.class)
+public interface EndpointMapper {
+
+    @Mapping(target = "secret", ignore = true)
+    Endpoint mapEntity(EndpointRequestDto source);
+
+    @Mapping(target = "isActive", source = "active")
+    EndpointResponseDto mapDto(Endpoint source);
+
+    List<Endpoint> mapEntityList(List<EndpointRequestDto> source);
+
+    List<EndpointResponseDto> mapDtoList(List<Endpoint> source);
+
+
+
 }
--- src/main/java/com/osigie/rehook/mapper/SubscriptionMapper.java
+++ src/main/java/com/osigie/rehook/mapper/SubscriptionMapper.java
@@ -1,4 +1,18 @@
 package com.osigie.rehook.mapper;
 
-public class SubscriptionMapper {
+import com.osigie.rehook.configuration.MapstructMapperConfig;
+import com.osigie.rehook.domain.model.Subscription;
+import com.osigie.rehook.dto.request.SubscriptionRequestDto;
+import com.osigie.rehook.dto.response.SubscriptionResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(config = MapstructMapperConfig.class, uses = EndpointMapper.class)
+public interface SubscriptionMapper {
+
+    Subscription mapEntity(SubscriptionRequestDto source);
+
+    @Mapping(target = "endpoints", source = "endpoints")
+    SubscriptionResponseDto mapDto(Subscription source);
 }
